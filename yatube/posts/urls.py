2e363c440e309ccd740a88038123e7d5595de3c9
Yuri Pomazkin <1yunker@gmail.com>
2022-12-28 12:30:11 +0500

    Add static, namespace and other

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,10 +1,11 @@
 # posts/urls.py
 from django.urls import path
-
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('groups', views.group_list),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('groups', views.group_list, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
 
 # Create your views here.
 # Главная страница
-
-
 def index(request):
-    return HttpResponse('Главная страница')
+    # Адрес шаблона сохраним в переменную, это не обязательно, но удобно
+    template = 'base.html'
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    title = 'Последние обновления на сайте'
+    text = 'Это главная страница проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': text,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 
-# Страница со списком мороженого
+# Страница со списком постов
 def group_list(request):
-    return HttpResponse('Список групп')
+    template = 'posts/group_list.html'
+    title = 'Лев Толстой – зеркало русской революции.'
+    text = 'Лев Толстой'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
 
 
-# Страница с информацией об одном сорте мороженого;
+# Страница с информацией о группах проекта Yatube
 # view-функция принимает параметр pk из path()
 def group_posts(request, slug):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
     # return HttpResponse(f'Пост номер {slug}')
